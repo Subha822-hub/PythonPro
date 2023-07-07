b887e0e74a5a6fca2d86adedfff68f79a2a568b3
Subha822-hub <suseraj1974@gmail.com>
2023-07-07 15:22:41 +0530

    first commit

---
+++ Section_01/Day_01.py
@@ -0,0 +1,15 @@
+print("Welcome to the Band Name Generator.")
+print("What's name of the city you grew up in?")
+first_name = input()
+print("What's your pet's name?")
+last_name = input()
+print("Your band name could be "+first_name+" "+last_name)
+
+
+# Output
+# Welcome to the Band Name Generator.
+# What's name of the city you grew up in?
+# Bristol
+# What's your pet's name?
+# Rabbit
+# Your band name could be Bristol Rabbit
