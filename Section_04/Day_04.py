6bce34683dd21c223af6685248c44abfa0e2711c
Subha822-hub <suseraj1974@gmail.com>
2023-07-12 20:00:43 +0530

    Rock,Paper,Scissors

---
+++ Section_04/Day_04.py
@@ -0,0 +1,65 @@
+# Go to https://replit.com/@appbrewery/rock-paper-scissors-start?v=1
+
+import random
+
+user_input= input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+rock='''
+
+                _    
+               | |   
+ _ __ ___   ___| | __
+| '__/ _ \ / __| |/ /
+| | | (_) | (__|   < 
+|_|  \___/ \___|_|\_\
+
+
+'''
+
+paper='''
+
+ _ __   __ _ _ __   ___ _ __ 
+| '_ \ / _` | '_ \ / _ \ '__|
+| |_) | (_| | |_) |  __/ |   
+| .__/ \__,_| .__/ \___|_|   
+| |         | |              
+|_|         |_|   
+
+'''
+
+scissors='''
+
+   ____
+  / __ \
+ ( (__) |___ ___
+  \________,'   """""----....____
+   _______<  () dd       ____----'
+  / __   __`.___-----""""
+ ( (__) |
+  \____/
+
+
+'''
+
+values=[rock,paper,scissors]
+computer_choose=random.randint(0,2)
+user_input=int(user_input)
+print(computer_choose)
+if(user_input==computer_choose):
+    print(values[user_input])
+    print("\nComputer Chose\n")
+    print(values[computer_choose])
+    print("DRAW")
+elif user_input==0 and computer_choose==1 or user_input==1 and computer_choose==3 or user_input==3 and computer_choose==1:
+    print(values[user_input])
+    print("\nComputer Chose\n")
+    print(values[computer_choose])
+    print("You Lose")
+else:
+    print(values[user_input])
+    print("\nComputer Chose\n")
+    print(values[computer_choose])
+    print("You Win")
+
+
+
---
+++ Section_04/Treasure_Map.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+
+
+a=int(position[0])
+b=int(position[1])
+map[b-1][a-1]="X"
+
+
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
